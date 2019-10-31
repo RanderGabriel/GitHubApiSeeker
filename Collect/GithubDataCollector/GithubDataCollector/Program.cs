33b0c541628d69b6b952b9a052f6e566799e4074
RanderGabriel <randergabriel98@gmail.com>
2019-10-30 21:23:36 -0300

    Primeiro commit

---
+++ Collect/GithubDataCollector/GithubDataCollector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GithubDataCollector
+{
+    class Program
+    {
+        static readonly HttpClient _client = new HttpClient();
+        static void Main(string[] args)
+        {
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd("Rander");
+            HttpResponseMessage message = _client.GetAsync("https://api.github.com/users").Result;
+            Console.WriteLine(message.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
